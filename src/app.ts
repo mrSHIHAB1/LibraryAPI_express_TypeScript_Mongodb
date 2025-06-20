acb4a55daf45afe9087803219a01d5a19d4e87f4
mrSHIHAB1 <montasirr36@gmail.com>
2025-06-20 19:54:19 +0600

    book Crud

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import express,{Application,Request,Response} from 'express';
+import { bookroutes } from './app/controllers/book.controller';
+
+
+
 const app: Application = express();
+
+app.use(express.json())
+
+app.use("/api/books",bookroutes)
+
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!');
 });
+
 
 export default app;
 
---
+++ src/app/models/book.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, model } from 'mongoose';
+import { IBook } from '../interfaces/book.interface';
+
+
+const bookSchema = new Schema<IBook>(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: {
+      type: String,
+      required: true,
+      enum: ['FICTION', 'NON_FICTION', 'SCIENCE', 'HISTORY', 'BIOGRAPHY', 'FANTASY'],
+    },
+    isbn: { type: String, required: true, unique: true },
+    description: String,
+    copies: { type: Number, required: true, min: 0 },
+    available: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+bookSchema.methods.updateAvailability = function () {
+  this.available = this.copies > 0;
+};
+
+
+export const Book = model<IBook>('Book', bookSchema);
